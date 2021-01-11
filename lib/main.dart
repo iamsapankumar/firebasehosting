374ba7f7f4a0910d624ca2648734abf52a2749ba
Sapan Kumar Das <sapanindia@gmail.com>
2021-01-11 16:19:55 +0530

    Minor update

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter sapan'),
+      home: MyHomePage(title: 'Firebase Hosting Successfull'),
     );
   }
 }
